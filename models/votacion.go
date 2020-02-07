25ee720fe44058bec161b626d1263fd5d1b65095
BOTOOM <eddiazr@correo.udistrital.edu.co>
2020-02-07 15:52:01 -0500

    adicion basica de metodos get #4

--- models/votacion.go
+++ models/votacion.go
@@ -1,6 +1,29 @@
 package models
 
+import "github.com/astaxie/beego"
+
 // ObtenerTodasVotaciones ...
-func ObtenerTodasVotaciones() (votaciones []map[string]interface{}, outputError interface{}) {
-	return nil, nil
+func ObtenerTodasVotaciones() (votaciones map[string]interface{}, outputError interface{}) {
+	var votacionesCenso map[string]interface{}
+	error := GetJSONJBPM(beego.AppConfig.String("administrativa_amazon_jbpm_url")+"contratoSuscritoProxyService/dependencias_sic/", &votacionesCenso)
+	if error != nil {
+		return nil, error
+	} else {
+		return votacionesCenso, nil
+
+	}
+	// return nil, nil
+}
+
+// ObtenerTodasVotacionesID ...
+func ObtenerTodasVotacionesID(idVotacion string) (votaciones map[string]interface{}, outputError interface{}) {
+	var votacionesCenso map[string]interface{}
+	error := GetJSONJBPM(beego.AppConfig.String("administrativa_amazon_jbpm_url")+"contratoSuscritoProxyService/dependencias_sic/"+idVotacion, &votacionesCenso)
+	if error != nil {
+		return nil, error
+	} else {
+		return votacionesCenso, nil
+
+	}
+	// return nil, nil
 }
