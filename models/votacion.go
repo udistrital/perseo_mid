35f7f887a67ffe9909ec74318e4481b8afffa472
BOTOOM <eddiazr@correo.udistrital.edu.co>
2020-02-15 21:57:00 -0500

    get id  exitoso #4

--- models/votacion.go
+++ models/votacion.go
@@ -24,30 +24,36 @@ func ObtenerTodasVotaciones() (votaciones []map[string]interface{}, outputError 
 
 // ObtenerTodasVotacionesID ...
 func ObtenerTodasVotacionesID(idVotacion string) (votaciones []map[string]interface{}, outputError interface{}) {
-	var votacionesCenso []map[string]interface{}
-	// error := GetJSONJBPM(beego.AppConfig.String("administrativa_amazon_jbpm_url")+"contratoSuscritoProxyService/dependencias_sic/"+idVotacion, &votacionesCenso)
-	// if error != nil {
-	// 	return nil, error
-	// } else {
-	// 	return votacionesCenso, nil
+	// var votacionesCenso []map[string]interface{}
+	var votacionesCenso map[string]interface{}
+	error := GetJSONJBPM(beego.AppConfig.String("administrativa_amazon_jbpm_url")+beego.AppConfig.String("perseo_ns_service")+"votaciones_id/"+idVotacion, &votacionesCenso)
+	if error != nil {
+		return nil, error
+	}
+	votacionesArray := votacionesCenso["votacion_id"].(map[string]interface{})
+	ArrayDeVotaciones, errArray := GetElementoMaptoStringToMapArray(votacionesArray["votacion"])
+	if ArrayDeVotaciones != nil {
+		votacionesConvertido := conversionVotacionCliente(ArrayDeVotaciones)
+		return votacionesConvertido, nil
+	}
+	return nil, errArray
 
-	// }
-	votacionesCenso = append(votacionesCenso, map[string]interface{}{
-		"Id":             2,
-		"Nombre":         "prueba",
-		"Observacion":    "reolucion x",
-		"Año":            "2020-02-20T05:00:00.000Z",
-		"Fechaejecucion": "2020-02-14T05:00:00.000Z",
-		"Estado":         true,
-		"DocentesPlanta": true,
-		"DocentesVe":     true,
-		"Funcionarios":   false,
-		"Estudiantes":    true,
-		"Egresados":      true,
-		"Contratistas":   false,
-		"Exrectores":     false,
-	})
-	return votacionesCenso, nil
+	// votacionesCenso = append(votacionesCenso, map[string]interface{}{
+	// 	"Id":             2,
+	// 	"Nombre":         "prueba",
+	// 	"Observacion":    "reolucion x",
+	// 	"Año":            "2020-02-20T05:00:00.000Z",
+	// 	"Fechaejecucion": "2020-02-14T05:00:00.000Z",
+	// 	"Estado":         true,
+	// 	"DocentesPlanta": true,
+	// 	"DocentesVe":     true,
+	// 	"Funcionarios":   false,
+	// 	"Estudiantes":    true,
+	// 	"Egresados":      true,
+	// 	"Contratistas":   false,
+	// 	"Exrectores":     false,
+	// })
+	// return votacionesCenso, nil
 }
 
 // PostVotaciones ...
