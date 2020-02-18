8b2bb12e7f9d85844379f8f3505a8745e3c181d6
BOTOOM <eddiazr@correo.udistrital.edu.co>
2020-02-18 17:27:06 -0500

    json que se envia para post #4

--- models/peticion.go
+++ models/peticion.go
@@ -26,3 +26,45 @@ func GetJSONJBPM(urlp string, target interface{}) error {
 
 	return json.NewDecoder(r.Body).Decode(target)
 }
+
+// SendJSONJBPM ...
+func SendJSONJBPM(urlp string, trequest string, target interface{}, datajson interface{}) error {
+	b := new(bytes.Buffer)
+	if datajson != nil {
+		json.NewEncoder(b).Encode(datajson)
+	}
+	//proxyUrl, err := url.Parse("http://10.20.4.15:3128")
+	//http.DefaultTransport = &http.Transport{Proxy: http.ProxyURL(proxyUrl)}
+
+	client := &http.Client{}
+	req, err := http.NewRequest(trequest, urlp, b)
+	req.Header.Set("Accept", "application/json")
+
+	//Se intenta acceder a cabecera, si no existe, se realiza peticion normal.
+	defer func() {
+		//Catch
+		if r := recover(); r != nil {
+
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			if err != nil {
+				beego.Error("Error reading response. ", err)
+			}
+
+			defer resp.Body.Close()
+			json.NewDecoder(resp.Body).Decode(target)
+		}
+	}()
+
+	//try
+	// header := GetHeader()
+	// req.Header.Set("Authorization", header)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		beego.Error("Error reading response. ", err)
+	}
+
+	defer resp.Body.Close()
+	return json.NewDecoder(resp.Body).Decode(target)
+}
