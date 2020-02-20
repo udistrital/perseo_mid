00c48e766ae6809321678935ec1a6e34a90f9402
BOTOOM <eddiazr@correo.udistrital.edu.co>
2020-02-20 00:15:57 -0500

    controlador y elimincacion de metodos no necesarios #7

--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -16,42 +16,6 @@ func init() {
             Filters: nil,
             Params: nil})
 
-    beego.GlobalControllerRouter["github.com/udistrital/perseo_mid/controllers:ScriptController"] = append(beego.GlobalControllerRouter["github.com/udistrital/perseo_mid/controllers:ScriptController"],
-        beego.ControllerComments{
-            Method: "GetAll",
-            Router: `/`,
-            AllowHTTPMethods: []string{"get"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
-
-    beego.GlobalControllerRouter["github.com/udistrital/perseo_mid/controllers:ScriptController"] = append(beego.GlobalControllerRouter["github.com/udistrital/perseo_mid/controllers:ScriptController"],
-        beego.ControllerComments{
-            Method: "GetOne",
-            Router: `/:id`,
-            AllowHTTPMethods: []string{"get"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
-
-    beego.GlobalControllerRouter["github.com/udistrital/perseo_mid/controllers:ScriptController"] = append(beego.GlobalControllerRouter["github.com/udistrital/perseo_mid/controllers:ScriptController"],
-        beego.ControllerComments{
-            Method: "Put",
-            Router: `/:id`,
-            AllowHTTPMethods: []string{"put"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
-
-    beego.GlobalControllerRouter["github.com/udistrital/perseo_mid/controllers:ScriptController"] = append(beego.GlobalControllerRouter["github.com/udistrital/perseo_mid/controllers:ScriptController"],
-        beego.ControllerComments{
-            Method: "Delete",
-            Router: `/:id`,
-            AllowHTTPMethods: []string{"delete"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
-
     beego.GlobalControllerRouter["github.com/udistrital/perseo_mid/controllers:VotacionController"] = append(beego.GlobalControllerRouter["github.com/udistrital/perseo_mid/controllers:VotacionController"],
         beego.ControllerComments{
             Method: "Post",
